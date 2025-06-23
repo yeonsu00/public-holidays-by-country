40f72d5058138ac315a20b627c3e1e037e5fca86
Kim yeonsu <yeonsu0329@kyonggi.ac.kr>
2025-06-23 23:39:13 +0900

    Initial commit

---
+++ src/main/java/com/publicholidaysbycountry/PublicHolidaysByCountryApplication.java
@@ -0,0 +1,13 @@
+package com.publicholidaysbycountry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicHolidaysByCountryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublicHolidaysByCountryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/publicholidaysbycountry/PublicHolidaysByCountryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.publicholidaysbycountry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicHolidaysByCountryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
