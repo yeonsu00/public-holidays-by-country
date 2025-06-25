083d9a92f6ec769ae7eab1facc16752bc18ed1a5
Kim yeonsu <yeonsu0329@kyonggi.ac.kr>
2025-06-26 01:21:50 +0900

    feat: open api 설정 추가, OpenApiConfig 클래스 추가

---
+++ src/main/java/com/publicholidaysbycountry/config/OpenApiConfig.java
@@ -0,0 +1,21 @@
+package com.publicholidaysbycountry.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Public Holidays by Country API")
+                        .description("Public Holidays by Country API Documentation")
+                        .version("1.0.0"));
+    }
+
+}
+
