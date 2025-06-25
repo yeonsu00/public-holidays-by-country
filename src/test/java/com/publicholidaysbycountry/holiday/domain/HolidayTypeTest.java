6c8394a331ea7d486be5889d29a74339a2bb7898
Kim yeonsu <yeonsu0329@kyonggi.ac.kr>
2025-06-25 20:14:17 +0900

    test: HolidayType 테스트 코드 작성

---
+++ src/test/java/com/publicholidaysbycountry/holiday/domain/HolidayTypeTest.java
@@ -0,0 +1,60 @@
+package com.publicholidaysbycountry.holiday.domain;
+
+import static org.assertj.core.api.Assertions.*;
+
+import com.publicholidaysbycountry.global.exception.InvalidHolidayTypeException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class HolidayTypeTest {
+
+    @DisplayName("유효한 HolidayType이 생성된다.")
+    @Test
+    void from_validTypeName() {
+        // given
+        String validTypeName = "Public";
+
+        // when
+        HolidayType holidayType = HolidayType.from(validTypeName);
+
+        // then
+        assertThat(holidayType).isEqualTo(HolidayType.PUBLIC);
+    }
+
+    @DisplayName("존재하지 않는 HolidayType이 생성될 때 예외가 발생한다.")
+    @Test
+    void from_invalidTypeName() {
+        // given
+        String invalidTypeName = "InvalidType";
+
+        // when & then
+        assertThatThrownBy(() -> HolidayType.from(invalidTypeName))
+            .isInstanceOf(InvalidHolidayTypeException.class)
+            .hasMessageContaining("에 해당하는 공휴일 타입을 찾을 수 없습니다.");
+
+    }
+
+    @DisplayName("대소문자를 무시하고 HolidayType이 생성된다.")
+    @Test
+    void from_ignoreCase() {
+        // given
+        String typeName = "puBlic";
+
+        // when
+        HolidayType holidayType = HolidayType.from(typeName);
+
+        // then
+        assertThat(holidayType).isEqualTo(HolidayType.PUBLIC);
+    }
+
+    @DisplayName("getTypeName()은 문자열을 반환한다.")
+    @Test
+    void getTypeName() {
+        // when
+        String typeName = HolidayType.PUBLIC.getTypeName();
+
+        // then
+        assertThat(typeName).isEqualTo("Public");
+    }
+
+}
