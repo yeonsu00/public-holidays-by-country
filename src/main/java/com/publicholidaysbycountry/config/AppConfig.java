ccd194d65580ca78485fecf0a0eeced31ba80d14
Kim yeonsu <yeonsu0329@kyonggi.ac.kr>
2025-06-25 14:39:48 +0900

    fix: RestTemplate에서 text/json 응답 처리하도록 설정

--- src/main/java/com/publicholidaysbycountry/config/AppConfig.java
+++ src/main/java/com/publicholidaysbycountry/config/AppConfig.java
@@ -1,7 +1,11 @@
 package com.publicholidaysbycountry.config;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -9,7 +13,16 @@ public class AppConfig {
 
     @Bean
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+        RestTemplate restTemplate = new RestTemplate();
+
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        List<MediaType> supportedMediaTypes = new ArrayList<>(converter.getSupportedMediaTypes());
+        supportedMediaTypes.add(MediaType.valueOf("text/json"));
+        converter.setSupportedMediaTypes(supportedMediaTypes);
+
+        restTemplate.getMessageConverters().addFirst(converter);
+
+        return restTemplate;
     }
 
 }
