3520f8223c6f93514ec58887836d17e6a4a5bca4
Kim yeonsu <yeonsu0329@kyonggi.ac.kr>
2025-06-26 15:20:16 +0900

    refactor: 불필요한 import 삭제 및 메서드명 변경 - getHolidaysByFilter -> getHolidaysByYearAndCountry

--- src/main/java/com/publicholidaysbycountry/holiday/presentation/HolidayController.java
+++ src/main/java/com/publicholidaysbycountry/holiday/presentation/HolidayController.java
@@ -10,19 +10,16 @@ import com.publicholidaysbycountry.holiday.presentation.request.YearAndCountryFi
 import com.publicholidaysbycountry.holiday.presentation.response.HolidayResponseDTO;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
 import java.time.LocalDate;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -47,7 +44,7 @@ public class HolidayController {
 
     @Operation(summary = "연도별·국가별 필터 기반 공휴일 조회", description = "연도별·국가별 필터 기반으로 공휴일을 페이징 처리하여 조회합니다.")
     @GetMapping
-    public CommonApiResponse<List<HolidayResponseDTO>> getHolidaysByFilter(
+    public CommonApiResponse<List<HolidayResponseDTO>> getHolidaysByYearAndCountry(
             @Validated @ModelAttribute YearAndCountryFilterRequest request) {
         List<Integer> year = request.getYear();
         List<String> countryCode = request.getCountryCode();
