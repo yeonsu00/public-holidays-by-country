283696ce16b10f4abb8e5ca4bdb6caa2d6d346b1
Kim yeonsu <yeonsu0329@kyonggi.ac.kr>
2025-06-28 01:05:57 +0900

    refactor: CountryRepositoryTest에서 불필요한 어노테이션 삭제

--- src/test/java/com/publicholidaysbycountry/country/application/CountryRepositoryTest.java
+++ src/test/java/com/publicholidaysbycountry/country/application/CountryRepositoryTest.java
@@ -12,9 +12,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.test.context.ActiveProfiles;
 
-@ActiveProfiles("test")
 @DataJpaTest
 @Import(CountryRepositoryImpl.class)
 class CountryRepositoryTest {
